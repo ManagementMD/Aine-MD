d999ca99b6f057154cfb30e68cbe151c9ffdf57b
ManagementMD <108587442+ManagementMD@users.noreply.github.com>
2022-08-09 02:58:54 -0200

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Gopay • SPay」
+│ •  [0857-5681-2987]
+│ •  [0857-5681-2987]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
